f0f3c332a7ed5819c13857911324dcab510441d2
Antonio Caricati <140312553+TonyC98@users.noreply.github.com>
2023-09-29 15:41:53 +0700

    created route /
    created route / which logs 'Welcome to Google Search'

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express')
 //run the express function
 const app = express()
+//create routes
+app.get('/', (req, res) => {
+  res.send('Welcome to Google Search')
+})
 //keep the server active
 app.listen(4000, () => {
   console.log('active server')
